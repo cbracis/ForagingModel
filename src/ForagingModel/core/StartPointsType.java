edc89be463cd4217ab6ba684495ce58a360786db
Chloe Bracis <cbracis@uw.edu>
2020-02-27 22:35:33 -0700

    Complete implementation for supporting multiple foragers, including parameters ForagerNumber to specify how many and StartPointsType to specify starting location. Also randomize order of Schedulables being called so there isn't an dvantage to being the first forager, and separate moving and consumption.

---
+++ src/ForagingModel/core/StartPointsType.java
@@ -0,0 +1,16 @@
+package ForagingModel.core;
+
+public enum StartPointsType {
+		/**
+		 * The starting location is at the center of the landscape
+		 */
+		Center, 
+		/**
+		 * The starting location is random
+		 */
+		Random,
+		/**
+		 * The stating location is read from a file
+		 */
+		FromFile
+}
